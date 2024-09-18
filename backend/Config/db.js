43eff5fa73d4868e7bd623dc6641daf24d5d44d4
LynnAkinyi <78434875+LynnAkinyi@users.noreply.github.com>
2024-09-18 14:55:48 +0300

    Update submodule

--- backend/Config/db.js
+++ backend/Config/db.js
@@ -2,10 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/MYSPARESGUY", {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect("mongodb://127.0.0.1:27017/MYSPARESGUY");
         console.log("MongoDB connected.");
     } catch (error) {
         console.error("MongoDB connection error:", error);
