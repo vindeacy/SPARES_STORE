0dcd895f046590cb8c24541a4ec9385c7af2bbd1
vindeacy <deacydavids70@gmail.com>
2024-09-24 09:15:05 +0300

    productdetails

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/login.jsx';
 import RegistrationPage from './pages/Registration.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ProductDetails from './pages/ProductDetails';
 import './App.css'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegistrationPage />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
       </Routes>
     </Router>
   )
